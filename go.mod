10c2d216d4be508f76da5f2aee255b0a5612c61b
Marcus Hotz <info@marcus-hotz.de>
2020-01-09 00:25:54 +0100

    1st implementation

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/MarcusHotz/pwgen
+
+go 1.13
+
+require (
+	github.com/sethvargo/go-password v0.1.3
+	golang.org/x/crypto v0.0.0-20200108215511-5d647ca15757
+)
---
+++ pwgen.go
@@ -0,0 +1,38 @@
+// passwords.go
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"golang.org/x/crypto/bcrypt"
+	
+	"github.com/sethvargo/go-password/password"
+)
+
+func HashPassword(password string) (string, error) {
+    bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+    return string(bytes), err
+}
+
+func CheckPasswordHash(password, hash string) bool {
+    err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+    return err == nil
+}
+
+func main() {
+	// Generate a password that is 64 characters long with 10 digits, 10 symbols,
+  	// allowing upper and lower case letters, disallowing repeat characters.
+  	password, err := password.Generate(64, 10, 10, false, false)
+  	if err != nil {
+		log.Fatal(err)	
+  	}
+
+    hash, _ := HashPassword(password) // ignore error for the sake of simplicity
+
+    fmt.Println("Password:", password)
+    fmt.Println("Hash:    ", hash)
+
+    match := CheckPasswordHash(password, hash)
+    fmt.Println("Match:   ", match)
+}
