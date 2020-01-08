6120fd4525fee7b96f8d79f2aa2f3b9ddc6a4f8d
Marcus Hotz <info@marcus-hotz.de>
2020-01-09 00:35:03 +0100

    code format

--- pwgen.go
+++ pwgen.go
@@ -1,4 +1,3 @@
-// passwords.go
 package main
 
 import (
@@ -6,33 +5,33 @@ import (
 	"log"
 
 	"golang.org/x/crypto/bcrypt"
-	
+
 	"github.com/sethvargo/go-password/password"
 )
 
 func HashPassword(password string) (string, error) {
-    bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
-    return string(bytes), err
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
 }
 
 func CheckPasswordHash(password, hash string) bool {
-    err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
-    return err == nil
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
 
 func main() {
 	// Generate a password that is 64 characters long with 10 digits, 10 symbols,
-  	// allowing upper and lower case letters, disallowing repeat characters.
-  	password, err := password.Generate(64, 10, 10, false, false)
-  	if err != nil {
-		log.Fatal(err)	
-  	}
+	// allowing upper and lower case letters, disallowing repeat characters.
+	password, err := password.Generate(64, 10, 10, false, false)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    hash, _ := HashPassword(password) // ignore error for the sake of simplicity
+	hash, _ := HashPassword(password) // ignore error for the sake of simplicity
 
-    fmt.Println("Password:", password)
-    fmt.Println("Hash:    ", hash)
+	fmt.Println("Password:", password)
+	fmt.Println("Hash:    ", hash)
 
-    match := CheckPasswordHash(password, hash)
-    fmt.Println("Match:   ", match)
+	match := CheckPasswordHash(password, hash)
+	fmt.Println("Match:   ", match)
 }
